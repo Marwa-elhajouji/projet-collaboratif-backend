7df0568946ec0aa0a9ffc44b72251bb5c3074c1e
Marwa Elhajouji <marwa.elhajouji@gmail.com>
2025-06-30 03:10:28 +0200

    update initial data in data.sql

--- projet-collaboratif/src/main/resources/data.sql
+++ projet-collaboratif/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO utilisateur (nom, email, mot_de_passe) VALUES 
-('Alice Martin', 'alice@mail.com', 'alice123'),
-('Bob Durand', 'bob@mail.com', 'bob123');
+('Marwa Elhajouji', 'marwa@mail.com', '78965'),
+('test', 'test@mail.com', '8521');
 
 
 INSERT INTO projet (nom, description, date_debut, date_fin, utilisateur_id) VALUES
@@ -11,4 +11,4 @@ INSERT INTO projet (nom, description, date_debut, date_fin, utilisateur_id) VALU
 
 INSERT INTO tache (titre, description, date_debut, date_fin, statut, projet_id, utilisateur_id) VALUES
 ('Analyse besoin', 'Analyser besoins client', '2024-01-10', '2024-01-15', 'A_FAIRE', 1, 1),
-('Développement module X', 'Coder module', '2024-02-01', '2024-03-01', 'EN_COURS', 2, 2);
+('Développement ', 'Coder module', '2024-02-01', '2024-03-01', 'EN_COURS', 2, 2);
